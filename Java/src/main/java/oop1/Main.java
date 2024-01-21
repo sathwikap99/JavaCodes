afb811c79068a7cc0025665016ccc7e9060d06f5
sathwikap99 <sathwikaammu123@gmail.com>
2024-01-21 14:30:29 -0600

    oops

---
+++ Java/src/main/java/oop1/Main.java
@@ -0,0 +1,13 @@
+package oop1;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Sathwika");
+        System.out.printf(student.getName());
+        System.out.println();
+
+        }
+
+}
---
+++ Java/src/main/java/oop1/Student.java
@@ -0,0 +1,14 @@
+package oop1;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
