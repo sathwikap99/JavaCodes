b40b1e7e74f39612af0553337b345fd9128e7ac5
sathwikap99 <sathwikaammu123@gmail.com>
2024-01-29 12:36:23 -0600

    hashset

---
+++ Java/src/main/java/hashset/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+
+    public String removeDuplicates(String input) {
+        if (input == null) {
+            return null;
+        }
+
+        Set<Character> charSet = new HashSet<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (charSet.add(c)) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
