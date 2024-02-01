f193f3438d08303d9b7afa9392ba2f9e1d46a58d
sathwikap99 <sathwikaammu123@gmail.com>
2024-02-01 15:28:20 -0600

    functionalinterfaces

---
+++ Java/src/main/java/functionalinterface/SwapIntegers.java
@@ -0,0 +1,14 @@
+package functionalinterface;
+
+public class SwapIntegers {
+    public static void swapIntegers(int a, int b) {
+        // Perform the swap
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
